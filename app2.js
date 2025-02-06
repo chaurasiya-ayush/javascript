5884e4017c394e88f3fdcb252370137f67d26d53
Ayush-Chaurasiya <ayushchaurasiya9532951470@gmail.com>
2025-02-06 22:41:48 +0530

    second file

---
+++ app2.js
@@ -0,0 +1,15 @@
+const ayush ={
+    marks :345,
+    fullname :"aryabattacharya",
+    quality:"chutiyapa",
+    arya(){
+        console.log("Fullname :",this.fullname);
+        console.log("marks :",this.marks);
+        console.log("quality :",this.quality);
+    },
+    rahul :function(){
+        console.log("No information about Rahul");
+        
+        
+    }
+}
